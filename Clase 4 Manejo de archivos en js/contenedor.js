94fd8ef473d6302fafdb8b740b4a53865eb1082b
FBersachia <45185158+FBersachia@users.noreply.github.com>
2022-01-25 17:51:27 -0300

    Add files via upload

--- Clase 4 Manejo de archivos en js/contenedor.js
+++ Clase 4 Manejo de archivos en js/contenedor.js
@@ -1,44 +1,87 @@
 const fs = require('fs');
 const { userInfo } = require('os');
 const path = require('path/posix');
+const { json } = require('stream/consumers');
 
 const pathToProduct = './producto.json';
-class Contenedor{
-    constructor (archivo){
+class Contenedor {
+    constructor(archivo) {
         this.archivo = archivo;
     }
 
     save = async producto => {
         //recibe un objeto, lo guarda en el archivo, devuelve el id asignado
-        if(!producto.title || !producto.price || !producto.id) return {status:"error", error:"Faltan datos"};
-        try{
-            if(fs.existsSync(pathToProduct)){
-                
-            }else{ //el archivo no existe
+        if (!producto.title || !producto.price) return { status: "error", error: "Faltan datos" };
+        try {
+            if (fs.existsSync(pathToProduct)) {
+                let data = await fs.promises.readFile(pathToProduct, 'utf-8');
+                let products = JSON.parse(data);
+                let id = 1;
+                if (products[0]) id = products[products.length - 1].id + 1; //Esto evita el error de que no hay ningun producto
+                producto.id = id;
+                products.push(producto);
+                await fs.promises.writeFile(pathToProduct, JSON.stringify(products, null, 2))
+                return { status: "success", message: "Producto añadido" };
+
+            } else { //el archivo no existe
                 producto.id = 1;
-                await fs.promises.writeFile(pathToProduct, JSON.stringify(producto, null, 2));
-                return {status: "success", message: `Product created with id ${producto.id}`}
+                await fs.promises.writeFile(pathToProduct, JSON.stringify([producto], null, 2));
+                return { status: "success", message: `Product created with id ${producto.id}` }
             }
-        }catch(error){
-            return {status:"error", error: error};
+        } catch (error) {
+            return { status: "error", error: error };
         }
     }
 
-    getById = number =>{
-        //Recibe un id y devuelve el objeto con ese id, o null si no está
+    getById = async id => {  //Recibe un id y devuelve el objeto con ese id, o null si no está
+        if (fs.existsSync(pathToProduct)) {
+            let data = await fs.promises.readFile(pathToProduct, 'utf-8');
+            let products = JSON.parse(data);
+            let product = products.find(p => p.id === id);
+            if (product) {
+                return { status: "success", producto: product };
+            } else {
+                return { status: "error", error: "Producto no encontrado" };
+            }
+        }
     }
 
-    getAll  = () =>{
-        //devuelve un array con los objetos presentes en el archivo
+    getAll = async () => { //devuelve un array con los objetos presentes en el archivo
+        if (fs.existsSync(pathToProduct)) {
+            let data = await fs.promises.readFile(pathToProduct, 'utf-8');
+            let products = JSON.parse(data);
+            if (products[0]) {
+                return { status: "success", productos: products };
+            } else {
+                return { status: "error", error: "No hay productos" };
+            }
+        }
     }
 
-    deleteByID = number =>{
-        //elimina del archivo el objeto con el id buscado
+    deleteByID = async id => { //elimina del archivo el objeto con el id buscado
+        if (fs.existsSync(pathToProduct)) {
+            let data = await fs.promises.readFile(pathToProduct, 'utf-8');
+            let products = JSON.parse(data);
+            if (!id || id > products.length || id < 1) {
+                return { status: "error", error: "No existe ese producto" };
+            } else {
+                let newProducts = products.filter(p => p.id !== id);
+                await fs.promises.writeFile(pathToProduct, JSON.stringify(newProducts, null, 2))
+                return { status: "success", productos: newProducts, message: `El producto con id ${id} fue eliminado` };
+            }
+        }
     }
 
-    deleteAll = () =>{
-        //Elimina todos los objetos presentes en el archivo
+    deleteAll = async () => { //Elimina todos los objetos presentes en el archivo
+        if (fs.existsSync(pathToProduct)) {
+            await fs.promises.writeFile(pathToProduct, JSON.stringify([], null, 2))
+            return { status: "success", message: `Todos los productos fueron eliminados` };
+        }
     }
 }
+
+
+
+
 
 module.exports = Contenedor;
--- Clase 4 Manejo de archivos en js/index.js
+++ Clase 4 Manejo de archivos en js/index.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
-const contenedor = require('./contenedor.js');
+const Contenedor = require('./contenedor.js');
 
 const container = new Contenedor();
 
 
 let producto = {
-    "title": "Lapiz",
-    "price": 12.34,
-    "thumbnail": "https://cdn3.iconfinder.com/4"
+    "title": "Goma",
+    "price": 12.54,
+    "thumbnail": "https://cdn3.iconfinder.com"
 }
 
 container.save(producto).then(result=>console.log(result));
-
-
+//container.getAll().then(result=>console.log(result));
+//container.getById(2).then(result=>console.log(result));
+//container.deleteByID(4).then(result=>console.log(result));
+//container.deleteAll().then(result=>console.log(result));
 
