e5a35e47f3964c255e2e0bd09db561c1fd0551e4
FBersachia <45185158+FBersachia@users.noreply.github.com>
2022-02-09 17:58:28 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,13 @@
+//server config
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const server = app.listen(PORT, ()=>console.log(`Listening on ${PORT}`));
+
+//code
+const ProductManager = require('./Managers/Products.js');
+const productManager = new ProductManager();
+const productsRouter = require('./routes/Products');
+app.use(express.urlencoded({extended:true}))
+app.use(express.json);
+app.use('/pets', productsRouter);
